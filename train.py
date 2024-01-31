b8ad6d5f833a09ca0db007ea2b7ca5b529bd74e8
Abid Ali Awan <abidaliawan@rocketmail.com>
2024-02-01 00:34:42 +0500

    round error resolved

--- train.py
+++ train.py
@@ -49,11 +49,11 @@ predictions = pipe.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
 f1 = f1_score(y_test, predictions, average="macro")
 
-print("Accuracy: ", str(accuracy.round(2) * 100) + "%", "F1: ", f1.round(2))
+print("Accuracy: ", str(round(accuracy, 2) * 100) + "%", "F1: ", round(f1, 2))
 
 ## Write metrics to file
 with open("Results/metrics.txt", "w") as outfile:
-    outfile.write(f"\nAccuracy = {accuracy.round(2)}, F1 Score = {f1.round(2)}.")
+    outfile.write(f"\nAccuracy = {round(accuracy, 2)}, F1 Score = {round(f1, 2)}.")
 
 
 ## Confusion Matrix Plot
