3dfc46dda9a032c2066f5ad19184a20208b4552f
Abid Ali Awan <abidaliawan@rocketmail.com>
2024-02-01 03:14:12 +0500

    final run and clean up

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 
 ## Loading the Data
-drug_df = pd.read_csv("Data/drug200.csv")
+drug_df = pd.read_csv("Data/drug.csv")
 drug_df = drug_df.sample(frac=1)
 
 ## Train Test Split
