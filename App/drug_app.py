d5c94b5fc8d641da04b3b69af240c8f34c45b46b
Abid Ali Awan <abidaliawan@rocketmail.com>
2024-02-03 03:12:16 +0500

    added exmaples to the app

--- App/drug_app.py
+++ App/drug_app.py
@@ -11,13 +11,19 @@ def classifier(Age, Sex, BP, Cholesterol, Na_to_K):
 
 
 inputs = [
-    gr.Slider(15, 74, step=1, label="Age", default=30),
-    gr.Radio(["M", "F"], label="Sex", default="F"),
-    gr.Radio(["HIGH", "LOW", "NORMAL"], label="Blood Pressure", default="LOW"),
-    gr.Radio(["HIGH", "NORMAL"], label="Cholesterol", default="NORMAL"),
-    gr.Slider(6.2, 38.2, step=0.1, label="Na_to_K", default=13.2),
+    gr.Slider(15, 74, step=1, label="Age"),
+    gr.Radio(["M", "F"], label="Sex"),
+    gr.Radio(["HIGH", "LOW", "NORMAL"], label="Blood Pressure"),
+    gr.Radio(["HIGH", "NORMAL"], label="Cholesterol"),
+    gr.Slider(6.2, 38.2, step=0.1, label="Na_to_K"),
 ]
 outputs = [gr.Label(num_top_classes=5)]
+
+examples=[
+        [30, "M", "HIGH", "NORMAL", 15.4],
+        [35, "F", "LOW", "NORMAL", 8],
+        [50, "M", "HIGH", "HIGH", 34],
+    ]
 
 title = "Drug Classification"
 description = "Enter the details to correctly identify Drug type?"
@@ -26,6 +32,7 @@ gr.Interface(
     fn=classifier,
     inputs=inputs,
     outputs=outputs,
+    examples=examples,
     title=title,
     description=description,
 ).launch()
