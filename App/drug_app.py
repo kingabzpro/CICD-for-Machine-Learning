682b25a0859b3865c2eab891c376299aaab216ef
Abid Ali Awan <abidaliawan@rocketmail.com>
2024-02-08 20:20:23 +0500

    try to fix httpx.InvalidURL

--- App/drug_app.py
+++ App/drug_app.py
@@ -42,15 +42,8 @@ examples = [
 
 title = "Drug Classification"
 description = "Enter the details to correctly identify Drug type?"
-article = """<center>
+article = "This app is a part of the Beginner's Guide to CI/CD for Machine Learning. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
 
-[![GitHub Repo stars](https://img.shields.io/github/stars/kingabzpro/CICD-for-Machine-Learning)](https://github.com/kingabzpro/CICD-for-Machine-Learning)[![Follow me on HF](https://huggingface.co/datasets/huggingface/badges/resolve/main/follow-me-on-HF-md.svg)](https://huggingface.co/kingabzpro)
-
-**This app is a part of the Beginner's Guide to CI/CD for Machine Learning.**
-
-**It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions.**
-
-</center>"""
 
 gr.Interface(
     fn=predict_drug,
