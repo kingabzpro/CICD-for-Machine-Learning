12d75bc513da25f153404429ceb94336f22ad4ba
Abid Ali Awan <abidaliawan@rocketmail.com>
2024-02-17 11:41:10 +0500

    fixed ASCII character

--- App/drug_app.py
+++ App/drug_app.py
@@ -42,13 +42,7 @@ examples = [
 
 title = "Drug Classification"
 description = "Enter the details to correctly identify Drug type?"
-article = """
-<center>  
-
-This app is a part of the **[Beginner's Guide to CI/CD for Machine Learning](https://www.datacamp.com/tutorial/ci-cd-for-machine-learning)**.
-
-</center>
-"""
+article = "This app is a part of the **[Beginner's Guide to CI/CD for Machine Learning](https://www.datacamp.com/tutorial/ci-cd-for-machine-learning)**. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
 
 
 gr.Interface(
