2c7feff3b0dec5893345a75efc12dfeb56d36ab2
Abid Ali Awan <abidaliawan@tutamail.com>
2024-12-02 19:08:53 +0500

    Update drug_app.py

--- App/drug_app.py
+++ App/drug_app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 import skops.io as sio
 
-pipe = sio.load("./Model/drug_pipeline.skops", trusted=True)
+pipe = sio.load("./Model/drug_pipeline.skops", trusted=False)
 
 
 def predict_drug(age, sex, blood_pressure, cholesterol, na_to_k_ratio):
